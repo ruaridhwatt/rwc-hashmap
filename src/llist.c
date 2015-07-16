5843b5d78af4d8007c38ce6afd2eae1491a9c32c
Ruaridh Watt <ruaridh.watt@gmail.com>
2015-07-16 22:02:43 +0200

    Working hashmap implementation. Test program started.

---
+++ src/llist.c
@@ -0,0 +1,89 @@
+/*
+ ============================================================================
+ Name        : llist.c
+ Author      : Ruaridh Watt dv12rwt
+ Created     : 5th September 2014
+ Description : An implementation of a linked list. See llist.h for
+               documentation on individual functions.
+
+               List uses an empty head element.
+               Inspect returns data from from the element next to the position
+               pointer.
+               Inspecting the end element will return NULL.
+ ============================================================================
+ */
+
+#include "llist.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+llist *llist_empty(void (*dataFreeFunc)(void *)) {
+	llist *list = malloc(sizeof(llist));
+	if (list == NULL) {
+		return NULL;
+	}
+	list->head = malloc(sizeof(element));
+	if (list->head == NULL) {
+		return NULL;
+	}
+	list->head->next = NULL;
+	list->head->data = NULL;
+	list->free_func = dataFreeFunc;
+	return list;
+}
+
+bool llist_isEmpty(llist *list) {
+	return list == NULL || list->head->next == NULL;
+}
+
+element *llist_first(llist *list) {
+	return list->head;
+}
+
+element *llist_insert(element *pos, llist *list, void *data) {
+	element *newElement = malloc(sizeof(element));
+	if (newElement == NULL) {
+		return NULL;
+	}
+	newElement->data = data;
+	newElement->next = pos->next;
+	pos->next = newElement;
+	return pos;
+}
+
+void *llist_inspect(element *pos) {
+	if (llist_isEnd(pos)) {
+		return NULL;
+	}
+	return pos->next->data;
+}
+
+element *llist_next(element *pos) {
+	return pos->next;
+}
+
+bool llist_isEnd(element *pos) {
+	return pos->next == NULL;
+}
+
+element *llist_remove(element *pos, llist *list) {
+	element *el = pos->next;
+	if (el != NULL) {
+		pos->next = el->next;
+		if (list->free_func != NULL) {
+			list->free_func(el->data);
+		}
+		free(el);
+	}
+	return pos;
+}
+
+void llist_free(llist *list) {
+	element *pos = llist_first(list);
+	while (!llist_isEmpty(list)) {
+		pos = llist_remove(pos, list);
+	}
+	free(list->head);
+	free(list);
+}
+
---
+++ src/main.c
@@ -0,0 +1,135 @@
+/*
+ * main.c
+ *
+ *  Created on: May 10, 2015
+ *      Author: ruaridh
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hashmap.h"
+
+unsigned long string_hash_function(void *data) {
+	char *str = (char*) data;
+	unsigned long hash = 5381;
+	int i = 0;
+	char c = str[i];
+	i++;
+
+	while (c != '\0') {
+		hash = ((hash << 5) + hash) + c;
+		c = str[i];
+		i++;
+	}
+
+	return hash;
+}
+
+entry *create_entry(char *k, char *v) {
+	entry *en = malloc(sizeof(entry));
+	if (!en) {
+		return NULL;
+	}
+
+	en->key_size = strlen(k) + 1;
+	en->key = calloc(en->key_size, sizeof(char));
+	if (!en->key) {
+		free(en);
+		return NULL;
+	}
+	strcpy(en->key, k);
+
+	en->value = calloc(strlen(v) + 1, sizeof(char));
+	if (!en->value) {
+		free(en->key);
+		free(en);
+		return NULL;
+	}
+	strcpy(en->value, v);
+	return en;
+}
+
+void entry_free_func(void *data) {
+	entry *e = (entry *) data;
+	free(e->key);
+	free(e->value);
+	free(e);
+}
+
+int main(void) {
+	int res;
+	hashmap *hm = hashmap_empty(20, string_hash_function, entry_free_func);
+	if (hm == NULL) {
+		return EXIT_FAILURE;
+	}
+	entry *en;
+	printf("Inserting test entries...\n");
+	en = create_entry("cat", "goes meow");
+	res = hashmap_put(en, hm);
+	if (res) {
+		fprintf(stderr, "FAIL: OOM (Insert1)\n");
+		return EXIT_FAILURE;
+	}
+	en = create_entry("dog", "goes woof");
+	res = hashmap_put(en, hm);
+	if (res) {
+		fprintf(stderr, "FAIL: OOM (Insert2)\n");
+		return EXIT_FAILURE;
+	}
+	en = create_entry("bird", "goes tweet");
+	res = hashmap_put(en, hm);
+	if (res) {
+		fprintf(stderr, "FAIL: OOM (Insert3)\n");
+		return EXIT_FAILURE;
+	}
+	en = create_entry("mouse", "goes sqeak");
+	res = hashmap_put(en, hm);
+	if (res) {
+		fprintf(stderr, "FAIL: OOM (Insert4)\n");
+		return EXIT_FAILURE;
+	}
+	if (hm->nr_entries == 4) {
+		printf("Success: 4 test entries inserted\n");
+	} else {
+		fprintf(stderr, "FAIL: Wrong number of entries! (insert5)\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("\nTest Get...\n");
+	char *key = "dog";
+	char *value = (char *) hashmap_get(key, strlen(key) + 1, hm);
+	if (value) {
+		printf("Success: %s %s\n", key, value);
+	} else {
+		fprintf(stderr, "FAIL: No entry found! (Get1)\n");
+		return EXIT_FAILURE;
+	}
+	value = (char *) hashmap_get(key, strlen(key), hm);
+	if (value) {
+		fprintf(stderr, "FAIL: Entry found! (Get2)\n");
+		return EXIT_FAILURE;
+	} else {
+		printf("Success: No entry found for erroneous key length\n");
+	}
+
+	printf("\nTest Remove...\n");
+	res = hashmap_remove(key, strlen(key) + 1, hm);
+	if (res) {
+		printf("Success: Entry removed\n");
+	} else {
+		fprintf(stderr, "FAIL: Entry not removed! (Remove1)\n");
+		return EXIT_FAILURE;
+	}
+	value = (char *) hashmap_get(key, strlen(key), hm);
+	if (value) {
+		fprintf(stderr, "FAIL: Entry found! (Remove2)\n");
+		return EXIT_FAILURE;
+	} else {
+		printf("Success: No entry found after removal\n");
+	}
+
+	hashmap_free(hm);
+	return EXIT_SUCCESS;
+}
+
